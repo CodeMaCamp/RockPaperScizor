095a5319acd7bcb70fb3eeba718074d7488f822a
Adam Ifnou <adamifnou@gmail.com>
2022-02-04 03:57:40 +0100

    second for gui

--- script.js
+++ script.js
@@ -1,81 +1,100 @@
-function computerPlay(){
-  let ran = Math.ceil( Math.random()*3);
-    if(ran==1){
-        return "rock";
-    }else if(ran==2){
-        return "paper";
-    }else if (ran ==3){
-        return "scizor";
-    }else{
-        return "weird thing happened with math.random";
-    }
+var plySel = "",
+    comSel = "",
+    score = 0,
+    count = 0;
+function computerPlay() {
+  let ran = Math.ceil(Math.random() * 3);
+  if (ran == 1) {
+    return "rock";
+  } else if (ran == 2) {
+    return "paper";
+  } else if (ran == 3) {
+    return "scizor";
+  } else {
+    return "weird thing happened with math.random";
+  }
 }
-function round(plySel, comSel){
-    plySel = plySel.toLowerCase();
-    comSel = comSel.toLowerCase();
-    if(plySel=="rock"){
-        if(comSel=="rock"){
-            return "Tie";
-        }else if(comSel=="paper"){
-            return "You Lose";
-        }else if(comSel=="scizor"){
-            return "You Win";
-        }else{
-            return "Wrong Input";
-        }
+function round(plySel, comSel) {
+  plySel = plySel.toLowerCase();
+  comSel = comSel.toLowerCase();
+  if (plySel == "rock") {
+    if (comSel == "rock") {
+      return "Tie";
+    } else if (comSel == "paper") {
+      return "You Lose";
+    } else if (comSel == "scizor") {
+      return "You Win";
+    } else {
+      return "Wrong Input";
     }
-    if(plySel=="paper"){
-        if(comSel=="rock"){
-            return "You Win";
-        }else if(comSel=="paper"){
-            return "Tie";
-        }else if(comSel=="scizor"){
-            return "You Lose";
-        }else{
-            return "Wrong Input";
-        }
+  }
+  if (plySel == "paper") {
+    if (comSel == "rock") {
+      return "You Win";
+    } else if (comSel == "paper") {
+      return "Tie";
+    } else if (comSel == "scizor") {
+      return "You Lose";
+    } else {
+      return "Wrong Input";
     }
-    if(plySel=="scizor"){
-        if(comSel=="rock"){
-            return "You Lose";
-        }else if(comSel=="paper"){
-            return "You Win";
-        }else if(comSel=="scizor"){
-            return "Tie";
-        }else{
-            return "Wrong Input";
-        }
-    }else{
-        return "wrong input";
+  }
+  if (plySel == "scizor") {
+    if (comSel == "rock") {
+      return "You Lose";
+    } else if (comSel == "paper") {
+      return "You Win";
+    } else if (comSel == "scizor") {
+      return "Tie";
+    } else {
+      return "Wrong Input";
     }
+  } else {
+    return "wrong input";
+  }
 }
-function game(){
-    let count = 0;
-    let score = 0;
-    do{
-        let  plySel = String(prompt("Type your hand, Rock - Paper - Scizor"));
-        let comSel = computerPlay();
-        console.log(round(plySel,comSel));
-        if(round(plySel,comSel)=="wrong input"){
-            count--;
-        }
-        if(round(plySel,comSel)=="You Win"){
-            score++;
-        }if(round(plySel,comSel)=="You Lose"){
-            score--;
-        }
-        count++;
-    }while(count<5);
-    if(score>0){
-        console.log("You Won In The Five Rounds");
-        return;
-    }else if(score<0){
-        console.log("You Lost In The Five Rounds");
-        return;
-    }else{
-        console.log("You Tied In The Five Rounds");
-        return;
+function playerPlay(string) {
+  plySel = document.getElementById(string).value;
+  comSel = computerPlay();
+  console.log(plySel + " " + score+' '+ count+" "+ round(plySel, comSel));
+  game();
+}
+function game() {
+  if (count < 5) {
+    console.log(round(plySel, comSel));
+    if (round(plySel, comSel) == "wrong input") {
+      count--;
     }
+    if (round(plySel, comSel) == "You Win") {
+      score++;
+      document.getElementById("result").innerHTML = "Computer played " + comSel.toUpperCase() + "You played " + plySel.toUpperCase() + " therefore You Won this round";
+    }
+    if (round(plySel, comSel) == "You Lose") {
+      score--;
+      document.getElementById("result").innerHTML = "Computer played " + comSel.toUpperCase() + " You played " + plySel.toUpperCase() + " therefore You lost this round";
+    }if(round(plySel,comSel) == "Tie"){
+      document.getElementById("result").innerHTML = "Computer played " + comSel.toUpperCase() + " You played " + plySel.toUpperCase() + " therefore You Tied this round";
+    }
+    count++;
+  } else if(count==5) {
+    if (score > 0) {
+      document.getElementById("result").innerHTML = "YOU WON THE GAME!";
+      score=0;
+      count=0;
+      return;
+    } else if (score < 0) {
+      document.getElementById("result").innerHTML = "YOU LOST THE GAME!";
+      score=0;
+      count=0;
+      return;
+    } else {
+      document.getElementById("result").innerHTML = "YOU TIED THE GAME!";
+      score=0;
+      count=0;
+      return;
+    }
+  }else{
+    return;
+  }
 }
 
-game();
